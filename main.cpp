59d560cd9eeb2edef1fc87015e0dcd985ec25702
Adeniji Charles Ayodipupo <adenijiayocharles@gmail.com>
2023-09-13 14:47:29 +0100

    added login code

--- main.cpp
+++ main.cpp
@@ -45,6 +45,44 @@ public:
         return password;
     }
 
+    void loginUser()
+    {
+        std::string filePath = currentDirectory + "/users.txt";
+        std::ifstream myFile;
+
+        myFile.open(filePath);
+        if (myFile.is_open())
+        {
+            std::string lineContent;
+            bool usernameFound = false;
+            bool passwordFound = false;
+
+            while (getline(myFile, lineContent))
+            {
+                if (lineContent.find(username) != -1)
+                {
+                    usernameFound = true;
+                }
+
+                if (lineContent.find(password) != -1)
+                {
+                    passwordFound = true;
+                }
+            }
+
+            if (usernameFound && passwordFound)
+            {
+                std::cout << "Login successful" << std::endl;
+            }
+            else
+            {
+                std::cout << "Haa haa" << std::endl;
+            }
+
+            myFile.close();
+        }
+    }
+
     void registerUser()
     {
         std::string filePath = currentDirectory + "/users.txt";
@@ -54,8 +92,8 @@ public:
 
         if (myFile.is_open())
         {
-            myFile << "username: " << getUsername() << std::endl;
-            myFile << "password: " << getPassword() << std::endl;
+            myFile << "username: " << username << std::endl;
+            myFile << "password: " << password << std::endl;
             std::cout << "=====";
             std::cout << "Registration successful";
             myFile.close();
@@ -76,6 +114,18 @@ int main()
 
     if (options == Actions::Login)
     {
+        std::string username;
+        std::cout << "Enter your username: ";
+        std::cin >> username;
+
+        std::string password;
+        std::cout << "Enter your password: ";
+        std::cin >> password;
+
+        account.setUsername(username);
+        account.setPassword(password);
+
+        account.loginUser();
     }
     else if (options == Actions::Register)
     {
